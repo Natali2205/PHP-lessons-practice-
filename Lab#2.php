ecd4c70c63e707fefbd3759772ffcb897e6deaab
Natalia <Natalia.Vasilets@gmail.com>
2017-08-18 23:28:55 +0300

    Solution changed

--- Lab#2.php
+++ Lab#2.php
@@ -1,73 +1,60 @@
 <?php
-<?
-php
 //1
 $str = 'var_test_text';
-$arr = explode('_', $str);
-$str2 = '';
-	foreach ($arr as $key => $val) {
-        if ($key != 0) {
-            $str2 .= ucfirst($val);
-        } else {
-            $str2 .= $val;
-        }
-    }
+$modify = lcfirst(str_replace('_', '', ucwords($str, '_')));
+echo lcfirst($modify);
 echo "1). Перетворити рядок 'var_test_text' в 'varTestText'." . '<br>' . "Розв'язок: ";
-echo $str2 . '<hr>';
+echo lcfirst($modify);
+'<hr>';
 
 //2
-$str = array();
-$str[0] = 'Ф'; 
-$str[1] = 'Ы';
-$str[2] = 'В';
-$str[3] = 'А';
-$str[4] = ' ';
-$str[5] = 'о';
-$str[6] = 'л';
-$str[7] = 'д';
-$str[8] = 'ж';
-
 echo "2)  Є рядок наступного вигляду: 'ФЫВА олдж'. 
 	Треба перетворити на наступний рядок 'АВЫФ ждло' utf-8" . '<br>' . "Розв'язок: ";
-
-for ($i = 4; $i >= 0; $i--) {
-    echo $str[$i];
+$str = 'ФЫВА олдж';
+$str1 = substr($str, 0, 8);
+$str2 = substr($str, -8, 8);
+function change($s)
+{
+    preg_match_all('/./u', $s, $ar);
+    return join('', array_reverse($ar[0]));
 }
-echo $str[4];
-for ($i = 8; $i > 3; $i--) {
-    echo $str[$i];
-}
-echo '<hr>';
+echo change($str1) . ' ' . change($str2);
 
 //3
 echo "3)  Розв'язок: " . '<br>';
 $a = [342, 55, 33, 123, 66, 63, 9];
-	foreach ($a as $val) {
-        if (strpos($val, '3') !== false) {
-            echo $val . '<br>';
-        }
+foreach ($a as $val) {
+    if (strpos($val, '3') !== false) {
+        echo $val . '<br>';
     }
+}
 echo '<hr>';
 
 //4
 echo "4)  Розв'язок: " . '<br>';
 $a = [342, 55, 33, 123, 66, 63, 9];
-	$i = 0;
-	foreach ($a as $val) {
-        if (strpos($val, '3') !== false) {
-            $i++;
-        }
+$i = 0;
+foreach ($a as $val) {
+    if (strpos($val, '3') !== false) {
+        $i += substr_count($val, '3');
     }
-	echo $i;
+}
+echo "всього чисел з '3': " . $i;
 
 echo '<hr>';
+$a = [342, 55, 33, 123, 66, 63, 9];
+$fileterd = array_filter($a, function ($value) {
+    if (strpos($value, '3') !== FALSE) {
+        echo $value . ' ';
+    }
+});
 
 //5
 echo "4)  Розв'язок: " . '<br>';
-	$str = 'super_girls';
-	$check1 = substr($str, -1);
-	$check2 = substr($str, 0, 1);
-    $str2 = substr($str, 1);
+$str = 'super_girls';
+$check1 = substr($str, -1);
+$check2 = substr($str, 0, 1);
+$str2 = substr($str, 1);
 if ($check2 == $check1) {
     echo ucfirst($str) . $str2;
 } else {
@@ -79,7 +66,7 @@ echo '<hr>';
 //6
 echo "4)  Розв'язок: " . '<br>';
 $str = 'ATTGC';
-	echo strtr($str, ['A' => 'T', 'T' => 'A', 'G' => 'C', 'C' => 'G']) . '<br>';
+echo strtr($str, ['A' => 'T', 'T' => 'A', 'G' => 'C', 'C' => 'G']) . '<br>';
 
 $str2 = 'GTAT';
-	echo strtr($str2, ['G' => 'C', 'T' => 'A', 'A' => 'T', 'T' => 'A']);
+echo strtr($str2, ['G' => 'C', 'T' => 'A', 'A' => 'T', 'T' => 'A']);
